75cd31be5c7974df1ba92d7530e8fea1a5dd4e9e
Aleksey Kurylev <alekseykurylev@yandex.ru>
2023-01-10 21:40:25 +0300

    feat: add ant.design

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Button } from "antd";
+import "antd/dist/reset.css";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>App</h1>
+      <Button type="primary">Button</Button>
+    </div>
+  );
 };
 
 export default App;
